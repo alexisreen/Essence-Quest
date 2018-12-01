ff6e5b585252b3374e032bce8138a00df40e74bf
alexisreen <41760038+alexisreen@users.noreply.github.com>
2018-12-01 21:05:58 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,10 +1,10 @@
-# Game: Search for the draconic essence
-# Creator: Lewis Murray (Anarchy)
+# Game: Essence Quest
+# Creator: Moro[CB4GANG]#9815 
 # Date: November 30 2018
 # Please do not steal, this is my first python project. :)
 import time
 import random
-from user_stats import Fireball, User, SilverSword
+from user_stats import User, SilverSword, Fireball
 from mob import Orc
 
 
@@ -13,15 +13,13 @@ def Begin():
     def Intro():
         dialogue = ["Old man: You have come far traveler.",
                     "Old man: What brings you so far from home?",
-                    "You: I have come in search of draconic essence.",
-                    "Old man: Draconic essence?, my, my, my... You must be very worthy to obtain such an item."]
+                    "You: I have come in search of Warlocks Essence.",
+                    "Old man: Warlocks Essence?, my, my, my... You must be very worthy to obtain such an item."]
         for string in dialogue:
             print(string)
-            time.sleep(0.7)
-
+            time.sleep(0.8)
 
     Intro()
-
 
     def CharacterCreation():
         print("---------------------------")
@@ -31,9 +29,7 @@ def Begin():
         User.Name = name
         time.sleep(0.4)
 
-
     CharacterCreation()
-
 
     def PixieMeeting():
         dialogue = ["---------------------------",
@@ -44,11 +40,9 @@ def Begin():
                     "It's just a harmless Pixie."]
         for string in dialogue:
             print(string)
-            time.sleep(0.7)
-
+            time.sleep(0.8)
 
     PixieMeeting()
-
 
     def FireballSpell():
         dialogue = ["---------------------------",
@@ -57,11 +51,9 @@ def Begin():
                     "+ NEW SPELL UNLOCKED: The Fireball Spell can do anywhere from 5-10 damage and cost 2-5 mana. +"]
         for string in dialogue:
             print(string)
-            time.sleep(0.7)
-
+            time.sleep(0.8)
 
     FireballSpell()
-
 
     def FireballTesting():
         dialogue = ["---------------------------",
@@ -71,58 +63,62 @@ def Begin():
                     "Your health is 100 and your mana is 50"]
         for string in dialogue:
             print(string)
-            time.sleep(0.7)
-
+            time.sleep(0.8)
 
     FireballTesting()
-
 
     def OrcAttack():
         dialogue = ["--------------------------",
                     "You come across an Orc!",
                     "The Orc has " + str(Orc.health) + " health and " + str(Orc.attack_damage) + " damage!",
-                    "You're health is: %i" % User.health,
                     "Do you wish to attack? Y/N"]
         for string in dialogue:
             print(string)
-            time.sleep(0.7)
-
+            time.sleep(0.8)
 
         if input('>> ') == "Y":
-            user_total_damage = 0
-            orc_total_damage = 0
-            while Orc.health > 5 or User.health > 0:
-                user_total_damage += SilverSword.attack_damage
-                orc_total_damage += Orc.attack_damage
-                Orc.health -= SilverSword.attack_damage
-                User.health -= Orc.attack_damage 
-                if User.health < 60:
-                    BoostUser.HealthManaBoost()
-                print("Increased your health by: %i" % (User.health-orc_total_damage))
-                if Orc.health < user_total_damage:
-                    print("Great! Orc Died\n")
+            User_total_damage = 0
+            Orc_total_damage = 0
+            orc_damage = 0
+            human_damage = 0 
+            while Orc.health > 0 or User.health > 0:
+                User_total_damage += SilverSword.attack_damage
+                print("%s: Silver Sword!" % User.Name)
+                User_total_damage += Starfall.attack_damage
+                print("%s: Star Fall!" % User.Name)
+                Orc_total_damage += Orc.attack_damage
+                print("Orc: Attack!")
+                human_damage = User_total_damage
+                orc_damage = Orc_total_damage
+                Orc.health -= human_damage
+                User.health -= orc_damage
+                if Orc.health < User_total_damage:
+                    print("Great! Orc Died")
                     break
-                if User.health < orc_total_damage:
-                    print("Crap! %s Died\n", User.Name)
+                if User.health < Orc_total_damage:
+                    print("Oh no %s Died", User.name)
                     break
-                print("You done "+str(user_total_damage)+" damage, the Orc's health is now "+str(Orc.health)+", %s's health is now "%str(User.Name) + str(User.health)+ ".")
+                print("You done %i damage." % human_damage , " The Orc done %i damage." % orc_damage, "the Orc's health is now " + str(Orc.health) + ", %s's health is now " % str(User.name) + ".")
+                orc_damage = 0
+                human_damage = 0
 
-            print("You've done %i damage"%user_total_damage+"\nThe Orc done %i damage\n" % orc_total_damage  + "%s's health: " % str(User.Name) + str(User.health) + ".")
-
-
+            print("You've done %i damage" % User_total_damage + "\nThe Orc done %i damage\n" % Orc_total_damage + "%s's health: " % str(User.Name) + str(User.health) + ".")
+           
     OrcAttack()
 
+
 def TitleScreen():
-    dialogue = ["  ___                  _      ___          _____ _          ___                       _      ___                        ",
-                " / __| ___ __ _ _ _ __| |_   | __|__ _ _  |_   _| |_  ___  |   \\ _ _ __ _ __ ___ _ _ (_)__  | __|______ ___ _ _  __ ___ ",
-                " \\__ \\/ -_) _` | '_/ _| ' \\  | _/ _ \\ '_|   | | | ' \\/ -_) | |) | '_/ _` / _/ _ \\ ' \\| / _| | _|(_-<_-</ -_) ' \\/ _/ -_)",
-                " |___/\\___\\__,_|_| \\__|_||_| |_|\\___/_|     |_| |_||_\\___| |___/|_| \\__,_\\__\\___/_||_|_\\__| |___/__/__/\\___|_||_\\__\\___|"]
+    dialogue = ["    ______                                  ____                  __ ",
+                "   / ____/____________  ____  ________     / __ \\__  _____  _____/ /_",
+                "  / __/ / ___/ ___/ _ \\/ __ \\/ ___/ _ \\   / / / / / / / _ \\/ ___/ __/",
+                " / /___(__  |__  )  __/ / / / /__/  __/  / /_/ / /_/ /  __(__  ) /_  ",
+                "/_____/____/____/\\___/_/ /_/\\___/\\___/   \\___\\_\\__,_/\\___/____/\\__/  "]
     for string in dialogue:
         print(string)
         time.sleep(0.4)
 
     print("--------------------------")
-    choice = input("WOULD YOU LIKE TO BEGIN YOUR JOURNEY? - Y/N ")
+    choice = input("WOULD YOU LIKE TO BEGIN YOUR JOURNEY? - Y/N >> ")
     if choice == 'Y':
         Begin()
     elif choice == 'N':
@@ -130,5 +126,6 @@ def TitleScreen():
     else:
         print("Invalid Option")
     print("--------------------------")
+
 
 TitleScreen()
